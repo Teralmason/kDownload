6cae84c4292bce8b46a70d689ba8a5dc99cd460f
Mehmet Emin Kartal <113544524+Teralmason@users.noreply.github.com>
2026-07-19 12:51:53 +0300

    Implement MyTileService for quick settings tile

---
+++ app/src/main/java/com/otomatik/indirici/MyTileService.kt
@@ -0,0 +1,24 @@
+package com.otomatik.indirici
+
+import android.service.quicksettings.TileService
+import android.widget.Toast
+
+class MyTileService : TileService() {
+
+    // Butona tıklandığında çalışacak olan fonksiyon
+    override fun onClick() {
+        super.onClick()
+        
+        // Şimdilik test için ekrana bir mesaj bastıralım
+        Toast.makeText(applicationContext, "Butona basıldı! Servis tetikleniyor...", Toast.LENGTH_SHORT).show()
+        
+        // TODO: Burada Erişilebilirlik Servisine (Accessibility) sinyal gönderip linki yakalayacağız.
+    }
+
+    // Buton durum çubuğuna eklendiğinde
+    override fun onTileAdded() {
+        super.onTileAdded()
+        tile.label = "Videoyu İndir"
+        tile.updateTile()
+    }
+}
