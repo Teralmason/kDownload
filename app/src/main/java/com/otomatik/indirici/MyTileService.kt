dacad12f9ae668378d05864a728b3ff8f0efa1d1
Mehmet Emin Kartal <113544524+Teralmason@users.noreply.github.com>
2026-07-19 13:17:05 +0300

    Fix tile label update and toast message display

--- app/src/main/java/com/otomatik/indirici/MyTileService.kt
+++ app/src/main/java/com/otomatik/indirici/MyTileService.kt
@@ -8,17 +8,17 @@ class MyTileService : TileService() {
     // Butona tıklandığında çalışacak olan fonksiyon
     override fun onClick() {
         super.onClick()
-        
+
         // Şimdilik test için ekrana bir mesaj bastıralım
         Toast.makeText(applicationContext, "Butona basıldı! Servis tetikleniyor...", Toast.LENGTH_SHORT).show()
-        
+
         // TODO: Burada Erişilebilirlik Servisine (Accessibility) sinyal gönderip linki yakalayacağız.
     }
 
     // Buton durum çubuğuna eklendiğinde
     override fun onTileAdded() {
         super.onTileAdded()
-        tile.label = "Videoyu İndir"
-        tile.updateTile()
+        qsTile.label = "Videoyu İndir"
+        qsTile.updateTile()
     }
 }
